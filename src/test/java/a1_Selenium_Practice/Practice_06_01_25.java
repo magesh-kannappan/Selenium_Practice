2901539ef3b9d7d09b60608b929408550ed0cf01
Magesh <mageshtesting2022@gmail.com>
2025-02-13 15:02:45 +0530

    Practice_13_02_25

--- src/test/java/a1_Selenium_Practice/Practice_06_01_25.java
+++ src/test/java/a1_Selenium_Practice/Practice_06_01_25.java
@@ -1,5 +1,9 @@
 package a1_Selenium_Practice;
 
+import java.io.File;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,8 +17,17 @@ public class Practice_06_01_25 {
 
 		driver.get("https://www.amazon.in/");
 		driver.switchTo().newWindow(WindowType.TAB);
-		
-		
+
+		// Take Screenshot
+		double di = 123.3344d;
+		int asd = (int) di;
+
+		byte age = 127;
+		short age2 = age;
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+
+		File srcFile = ts.getScreenshotAs(OutputType.FILE);
 
 	}
 
