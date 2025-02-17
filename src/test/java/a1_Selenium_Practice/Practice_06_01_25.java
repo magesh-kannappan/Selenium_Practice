32ae261e83daefbea4267f5dc3d5d8aaf537c1b6
Magesh <mageshtesting2022@gmail.com>
2025-02-17 17:20:55 +0530

    Practice_17_02_25

--- src/test/java/a1_Selenium_Practice/Practice_06_01_25.java
+++ src/test/java/a1_Selenium_Practice/Practice_06_01_25.java
@@ -15,6 +15,7 @@ public class Practice_06_01_25 {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
+		
 		driver.get("https://www.amazon.in/");
 		driver.switchTo().newWindow(WindowType.TAB);
 
