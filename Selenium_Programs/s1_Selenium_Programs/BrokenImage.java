1e2dd3ab8b27922a85352859b78789fafbe52bb6
Magesh <mageshtesting2022@gmail.com>
2025-03-13 17:51:17 +0530

    Practice_13_03_25

---
+++ Selenium_Programs/s1_Selenium_Programs/BrokenImage.java
@@ -0,0 +1,51 @@
+package s1_Selenium_Programs;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrokenImage {
+
+	public static void main(String[] args) {
+
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "https://omayo.blogspot.com/";
+		driver.get(baseUrl);
+		int totalImages = 0;
+
+		List<WebElement> images = driver.findElements(By.tagName("img"));
+		for (WebElement img : images) {
+
+			String domAttribute = img.getDomAttribute("src");
+			totalImages++;
+
+			if (domAttribute == null || domAttribute.isEmpty()) {
+				System.out.println("Image link is empty");
+				continue;
+			} else {
+
+				try {
+					URL url = new URL(baseUrl + domAttribute);
+					HttpURLConnection http = (HttpURLConnection) url.openConnection();
+					http.setConnectTimeout(3000);
+					http.connect();
+					int responseCode = http.getResponseCode();
+
+					if (responseCode == 200) {
+						System.out.println("Images is  not broken >>> " + baseUrl + domAttribute);
+					} else {
+						System.err.println("Image is broken >>> " + baseUrl + domAttribute);
+					}
+				} catch (Exception e) {
+				}
+			}
+		}
+		System.out.println("Total images count - " + totalImages);
+	}
+
+}
--- Selenium_Programs/s1_Selenium_Programs/Broken_Link.java
+++ Selenium_Programs/s1_Selenium_Programs/Broken_Link.java
@@ -1,11 +1,53 @@
 package s1_Selenium_Programs;
 
-import java.net.MalformedURLException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Broken_Link {
 
 	public static void main(String[] args) {
 
-	}
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.leafground.com/link.xhtml");
+		driver.manage().window().maximize();
 
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		String title = driver.getTitle();
+		int linkCount = 0;
+		System.out.println("Links found in the " + title + ":");
+
+		for (WebElement link : links) {
+
+			String domAttribute = link.getDomAttribute("href");
+//			System.out.println(domAttribute);
+
+			if (domAttribute == null || domAttribute.isEmpty()) {
+
+				try {
+					URL url = new URL(domAttribute);
+					linkCount++;
+					HttpURLConnection http = (HttpURLConnection) url.openConnection();
+					http.connect();
+					int responseCode = http.getResponseCode();
+
+					if (responseCode >= 400) {
+						System.err.println("The link is broken");
+					} else {
+						System.out.println("Link is working fine - " + url);
+						System.out.println("Total link count - " + linkCount);
+					}
+				} catch (Exception e) {
+
+				}
+
+			}
+		}
+		
+	}
 }
