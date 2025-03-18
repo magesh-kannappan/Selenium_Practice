bc747e1296a42ec584b4dc610c72ab8de37e7d90
Magesh <mageshtesting2022@gmail.com>
2025-03-18 18:48:07 +0530

    Laptop_18_03_25

--- Selenium_Programs/s3_Screenshot/Screenshot.java
+++ Selenium_Programs/s3_Screenshot/Screenshot.java
@@ -1,8 +1,8 @@
 package s3_Screenshot;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -18,13 +18,24 @@ public class Screenshot {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 
-		driver.get("https://www.netmeds.com/");
-		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
-		WebElement search = driver.findElement(By.id("search"));
-		search.sendKeys("Magesh");
-		File sourceScreenshot = search.getScreenshotAs(OutputType.FILE);
-		File Destination = new File("C:/Eclipse/Mini Project/SeleniumPractice/" + "Screenshot/screen.png");
-		FileHandler.copy(sourceScreenshot, Destination);
+		driver.get("https://letcode.in/edit");
+
+		WebElement container = driver.findElement(By.xpath("//div[@class='card']"));
+
+		File shot = container.getScreenshotAs(OutputType.FILE);
+		File dest = new File("C:\\Users\\NMSLAP511\\Downloads\\Shipping Charges update\\temp1.png");
+//		File asd = new File("C:\\Users\\NMSLAP511\\Downloads\\Shipping Charges update\\temp.jpg");
+
+		FileHandler.copy(shot, dest);
+		Desktop.getDesktop().open(dest);
+
+		// driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+		// WebElement search = driver.findElement(By.id("search"));
+		// search.sendKeys("Magesh");
+		// File sourceScreenshot = search.getScreenshotAs(OutputType.FILE);
+		// File Destination = new File("C:/Eclipse/Mini Project/SeleniumPractice/" +
+		// "Screenshot/screen.png");
+		// FileHandler.copy(sourceScreenshot, Destination);
 	}
 
 }
